3f1cc6cb9c37468c792b23a8ab3cd8a66bb4df2a
Ashish Belwase <belwasetech@Ashishs-MacBook-Pro.local>
2018-04-09 22:09:25 +0545

    fixed prime issue

--- primes.py
+++ primes.py
@@ -1,15 +1,16 @@
 def is_prime(number):
     """Return True if *number* is prime."""
-    for element in range(number):
+    for element in range(2, number):
+        #print ('el ', element)
         if number % element == 0:
             return False
 
     return True
 
-def print_next_prime(number):
-    """Print the closest prime number larger than *number*."""
-    index = number
-    while True:
-        index += 1
-        if is_prime(index):
-            print(index)
+# def print_next_prime(number):
+#     """Print the closest prime number larger than *number*."""
+#     index = number
+#     while True:
+#         index += 1
+#         if is_prime(index):
+#             print(index)
